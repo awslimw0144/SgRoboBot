79b1c410e3fe4ac4d98eda2ebe0881aaea9e6fe8
awslimw0144 <awslimw0144@gmail.com>
2021-02-12 17:37:05 +0800

    Mock extraction of data using wikipedia

---
+++ src/enum/list_of_pages.ts
@@ -0,0 +1,7 @@
+enum WIKIPEDIA {
+    LIST_OF_US_STATES = "https://en.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States"
+}
+
+export {
+    WIKIPEDIA
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
-const world:string = "Hello my super new world";
+import axios from 'axios';
+import { WIKIPEDIA } from './enum/list_of_pages';
 
-function hello(word:string=world):string{
-    return "Hello ${world}!"
+const page_url:string = WIKIPEDIA.LIST_OF_US_STATES;
+
+// Kickstart
+async function getStates_US(){
+    const {data} = await axios.get(page_url);
+    console.log(data);
 }
 
-export {
-    hello
-}
+// Mock Application
+getStates_US();
+
+// export {
+//     getStates_US
+// }
